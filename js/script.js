065d956cd89d5ea6bbfd4fdde32b177b7111946b
Giovanni Ciotta <giociotta@gmail.com>
2022-05-04 16:15:11 +0200

    sconti

--- js/script.js
+++ js/script.js
@@ -13,9 +13,25 @@ const eta = prompt(" Quanti anni hai?");
 
 console.log(eta);
 
-// prezzo scontato per minorenni
-
-// prezzo scontato per over65
-
 // prezzo finale
+const prezzoFinale = prezzoBiglietto * chilometri; 
 
+        console.log(prezzoFinale);
+
+// prezzo scontato per minorenni
+const prezzoMinorenni = prezzoBiglietto * 80 / 100;
+// prezzo scontato per over65
+const prezzoOver = prezzoBiglietto * 60 / 100;
+
+
+
+if ( eta < 18 ) {
+    
+    } 
+     else {const prezzoFinale = prezzoBiglietto * chilometri; 
+
+        console.log(prezzoFinale);
+    }
+
+
+ 
