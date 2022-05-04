1e6040096c01e16be51d6c5833234be51249fd46
Giovanni Ciotta <giociotta@gmail.com>
2022-05-04 15:33:40 +0200

    starter

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// prezzo biglietto
+let prezzoBiglietto = 0.21;
+
+//  chiediamo il numero di chilometri che vuole percorrere
+const chilometri = prompt("Quanti chilometri vuoi percorrere?");
+
+
+console.log(chilometri);
+
+// chiediamo eta passeggero
+
+const eta = prompt(" Quanti anni hai?");
+
+console.log(eta);
+
+// prezzo scontato per minorenni
+
+// prezzo scontato per over65
+
+// prezzo finale
+
